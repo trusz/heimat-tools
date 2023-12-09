04b3f9c6b9669aebecc6369f1dc6760eb1d19155
Tamás Russ <tamas.russ@tuta.io>
2023-12-09 12:11:32 +0100

    infra(01): setup

---
+++ packages/01_allocation_overview/vite.config.build.ts
@@ -0,0 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+// The plugin typeing is not correct, so we have to use @ts-nochec
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [svelte({
+		configFile:      "svelte.config.build.js",
+		compilerOptions: {
+			customElement: true,
+		},
+	})],
+	build: {
+		lib: {
+			entry:   "src/lib/index.ts",
+			fileName: "index",
+			formats: ["es"],
+		},
+	},
+})
+	
