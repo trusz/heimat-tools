978b2e73759532a0d976bd8988370ad7fcd6196e
Tamás Russ <github@tamasruss.com>
2024-09-16 10:57:04 +0200

    add deployment

--- packages/staffing/src/App.svelte
+++ packages/staffing/src/App.svelte
@@ -3,40 +3,58 @@
 </svelte:head>
 
 
-<h1>
-	Planned Positions
-</h1>
+<main>
+	<h1>
+		Planned Positions
+	</h1>
+
+
+	<span>
+		Searching projects {loaded_projects}/{all_projects}
+		<div class="progress">
+			<div class="progress-bar" role="progressbar" style="width: {progress_precent}%" aria-valuenow="{progress_precent}" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+	</span>
 
 {#if project_details_list.length > 0}
-	<table>
+	<table class="table table-bordered">
 		<thead>
 			<tr>
-				<th>Project</th>
-				<th>Role</th>
-				<th>Is Staffed</th>
+				<th scope="col">Project</th>
+				<th scope="col">Role</th>
+				<th scope="col">Is Staffed</th>
 			</tr>
 		</thead>
-	{#each project_details_list as project_details}
-		{#each project_details.allocations as allocation}
-			<tr style={row_style(allocation.is_staffed)}>
-				<td>{project_details.name}</td>
-				<td>{allocation.project_role}</td>
-				<td>{allocation.is_staffed}</td>
-			</tr>
+		<tbody>
+		{#each project_details_list as project_details}
+			{#each project_details.allocations as allocation}
+				<tr class={row_style(allocation.is_staffed)}>
+					<td>
+						<a href={`https://heimat.sprinteins.com/core/heimat/projects/${project_details.id}`}>
+							 {project_details.name} 
+						</a>
+					</td>
+					<td>{allocation.project_role}</td>
+					<td>{allocation.is_staffed}</td>
+				</tr>
+			{/each}
 		{/each}
-	{/each}
+	</tbody>
 	</table>
 {/if}
-
+</main>
 
 <style>
-	
+	main{
+		margin: 20px;
+	}
 </style>
 
 
 <script lang="ts">
     import type { Allocation, Project_Details } from "@heimtools/api";
 	import {api} from "./api"
+	import "./bootstrap.min.css"
     // import { waitForElement } from "./util";
 
 	// Load our style
@@ -44,15 +62,19 @@
     const cssURL = new URL("style.css", baseURL)
     const styleURL = cssURL.toString()
 
-	const urlAllProjects = "https://heimat.sprinteins.com/heimat-core/projects/overview/"
-
 	let project_details_list: Project_Details[] = []
+
+	let all_projects = 0
+	let loaded_projects = 0
+	$: progress_precent = all_projects?(loaded_projects / all_projects) * 100:0
 
 	async function init() {
 		const projects = await api.fetch_projects()
 		projects.sort((a, b) => b.id - a.id)
+		all_projects = projects.length
 		for(const project of projects) {
 			const project_details = await api.fetch_project_details(project.id)
+			loaded_projects++
 			project_details_list = [...project_details_list, project_details]
 		}
 	}
@@ -61,9 +83,9 @@
 
 	function row_style(is_staffed:boolean){
 		if(is_staffed){
-			return "color: green;"
+			return ""
 		}
-		return "color: red;"
+		return "table-danger"
 	}
 	
 </script>
